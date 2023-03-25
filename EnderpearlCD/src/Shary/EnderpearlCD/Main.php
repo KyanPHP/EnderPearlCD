d400b4aa560faeb5e4465cd562a10d54ef0516d2
KyanPHP <128573943+KyanPHP@users.noreply.github.com>
2023-03-25 11:53:41 +1100

    Update Main.php

--- EnderpearlCD/src/Shary/EnderpearlCD/Main.php
+++ EnderpearlCD/src/Shary/EnderpearlCD/Main.php
@@ -16,20 +16,19 @@ class Main extends PluginBase implements Listener {
     private $pearlcd = [];
     private $config;
 
-    public function onEnable() {
+    public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveResource("config.yml");
         $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
     }
 
-    public function onEnderPearl(PlayerInteractEvent $event)
-    {
+    public function onEnderPearl(PlayerInteractEvent $event): void {
         $item = $event->getItem();
         if($item->getId() === ItemIds::ENDER_PEARL) {
             $cooldown = $this->config->get("cooldown");
             $player = $event->getPlayer();
             if (isset($this->pearlcd[$player->getName()]) and time() - $this->pearlcd[$player->getName()] < $cooldown) {
-                $event->setCancelled();
+                $event->setCancelled(true);
                 $time = time() - $this->pearlcd[$player->getName()];
                 $message = $this->config->get("message");
                 $message = str_replace("{cooldown}", ($cooldown - $time), $message);
